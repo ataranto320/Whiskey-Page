b0e2b080d4af05966c07332b974830b02d939df1
Austin T <austin.taranto@gmail.com>
2020-04-27 16:19:48 -0400

    created json data list with some whiskey information for database search purposes. will work on and update when i decide which i want to do

---
+++ scotch/scotchJSON.js
@@ -0,0 +1,66 @@
+export const SCOTCH = [
+    {
+        name: "Macallan",
+        href: "https://www.themacallan.com/en-us",
+        location: "Scottland",
+        region: "Speyside",
+        type: "scotch",
+        image: []
+    },
+    {
+        name: "Ardbeg",
+        href: "https://www.ardbeg.com/en-US",
+        location: "Scottland",
+        region: "Islay",
+        type: "scotch",
+        image: []
+    },
+    {
+        name: "Balvenie",
+        href: "https://www.thebalvenie.com",
+        location: "Scottland",
+        region: "Speyside",
+        type: "scotch",
+        image: []
+    },
+    {
+        name: "Lagavulin",
+        href: "https://www.malts.com/en-us/our-whisky-collection/lagavulin/",
+        location: "Scottland",
+        region: "Islay",
+        type: "scotch",
+        image: []
+    },
+    {
+        name: "Highland Park",
+        href: "https://www.highlandparkwhisky.com/en-us",
+        location: "Scottland",
+        region: "Highland",
+        type: "scotch",
+        image: []
+    },
+    {
+        name: "Yamazaki",
+        href: "https://whisky.suntory.com/en/na/products/yamazaki",
+        location: "Japan",
+        region: "Japan",
+        type: "scotch",
+        image: []
+    },
+    {
+        name: "Hakushu",
+        href: "https://whisky.suntory.com/en/na/products/hakushu",
+        location: "Japan",
+        region: "Japan",
+        type: "scotch",
+        image: []
+    },
+    {
+        name: "Hibiki",
+        href: "https://whisky.suntory.com/en/na/products/hibiki",
+        location: "Japan",
+        region: "Japan",
+        type: "scotch",
+        image: []
+    },
+]
